6950db864711f1508f3ffd4951de8bcbaf1428c7
yo <yodai0903@gmail.com>
2025-10-20 23:32:58 +0900

    頑張って350Hzくらいの正弦波の出力に成功

---
+++ LraDynamicsBoardLARGE-SinWave350Hz/LraDynamicsBoardLARGE-SinWave350Hz.ino
@@ -0,0 +1,29 @@
+/*
+LraBoard_Largeのコードです．
+MCP4725ライブラリのI2Cのデフォルトは400kHzですが，800kHzで駆動させています．
+また，正弦波の分解能を16段階にすることで350Hzほどで正弦波が出力できます．
+*/
+
+#include <Adafruit_MCP4725.h>
+
+Adafruit_MCP4725 dac1;
+
+const PROGMEM uint16_t DACLookup_FullSine_5Bit[16] =
+{
+  2048, 2831, 3495, 3939,
+  4095, 3939, 3495, 2831,
+  2048, 1264,  600,  156,
+     0,  156,  600, 1264
+};
+
+
+void setup() {
+  dac1.begin(0x62);
+}
+
+void loop() {
+  uint16_t i;
+  for(i=0;i<16;i++){
+    dac1.setVoltage(pgm_read_word(&(DACLookup_FullSine_5Bit[i])),false,800000);
+  }
+}
