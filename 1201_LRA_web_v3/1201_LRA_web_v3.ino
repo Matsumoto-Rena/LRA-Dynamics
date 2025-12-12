47b7d848da1033e190550e8b0cb5c32971cea586
yod2004 <yodai0903@gmail.com>
2025-12-12 17:39:42 +0900

    コメント修正

--- 1201_LRA_web_v3/1201_LRA_web_v3.ino
+++ 1201_LRA_web_v3/1201_LRA_web_v3.ino
@@ -2,8 +2,8 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-const char* ssid = "(･∀･)";
-const char* password = "235711131719";
+const char* ssid = "" your wifi ssid
+const char* password = "" your wifi password 
 
 Adafruit_MCP4725 dac1;
 WebServer server(80);
