23dd8c860f94369073d02a7e5f59db057607d874
Matusmoto-Rena <rena30858@gmail.com>
2025-12-18 00:00:50 +0900

    送信タイミングを早める

--- Xiao_ESP32_C3/bluetooth_connection/bluetooth_connection.ino
+++ Xiao_ESP32_C3/bluetooth_connection/bluetooth_connection.ino
@@ -123,10 +123,10 @@ void runPatternStep(int id) {
   digitalWrite(D6, LOW); digitalWrite(D8, LOW); digitalWrite(D7, LOW); digitalWrite(D10, LOW);
   
   switch(id) {
-    case 1: digitalWrite(D6, HIGH);  for(uint8_t i=0; i < 10; i++) move(paramRes1, paramRep1, true);  break;
-    case 2: digitalWrite(D8, HIGH);  for(uint8_t i=0; i < 10; i++) move(paramRes1, paramRep1, false); break;
-    case 3: digitalWrite(D7, HIGH);  move(paramRes2, paramRep2, false);  break;
-    case 4: digitalWrite(D10, HIGH); move(paramRes2, paramRep2, true); break;
+    case 1: digitalWrite(D6, HIGH);  for(uint8_t i=0; i < 100; i++) move(paramRes1, paramRep1, true);  break;
+    case 2: digitalWrite(D8, HIGH);  for(uint8_t i=0; i < 100; i++) move(paramRes1, paramRep1, false); break;
+    case 3: digitalWrite(D7, HIGH);  for(uint8_t i=0; i < 100; i++) move(paramRes2, paramRep2, false);  break;
+    case 4: digitalWrite(D10, HIGH); for(uint8_t i=0; i < 100; i++) move(paramRes2, paramRep2, true); break;
   }
 }
 
