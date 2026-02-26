fbdb73bf73d909cac5eb2ce831032945081793aa
Matusmoto-Rena <rena30858@gmail.com>
2026-02-26 13:17:08 +0900

    黄色と赤丸を読むようにする

---
+++ xiao_moveLRA/.pio/build/seeed_xiao_esp32c3/lib577/BLE/BLEExceptions.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/seeed_xiao_esp32c3/lib577/BLE/BLEExceptions.cpp.o: \
+ C:/Users/m-ren/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
---
+++ xiao_moveLRA/.pio/build/seeed_xiao_esp32c3/lib577/BLE/BLESecurity.cpp.d
@@ -0,0 +1,8 @@
+.pio/build/seeed_xiao_esp32c3/lib577/BLE/BLESecurity.cpp.o: \
+ C:/Users/m-ren/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.cpp \
+ C:/Users/m-ren/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.h \
+ C:/Users/m-ren/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/qio_qspi/include/sdkconfig.h \
+ C:/Users/m-ren/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/include/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h \
+ C:/Users/m-ren/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/include/esp_common/include/esp_err.h \
+ C:/Users/m-ren/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/include/esp_common/include/esp_compiler.h \
+ C:/Users/m-ren/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/include/bt/host/bluedroid/api/include/api/esp_bt_defs.h
